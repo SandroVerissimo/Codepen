e5c2d921329714b1188929ea0e6f7ff2ff7d2dc0
Sandro Verissimo <Sandrogater@gmail.com>
2022-10-07 09:23:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var element = document.querySelector('#header');
+    element.classList.toggle("Dm1");
+
+    var element = document.querySelector('#Bg1');
+    element.classList.toggle("Dm2");
+
+    var element = document.querySelector('#Bg2');
+    element.classList.toggle("Dm3");
+
+    var element = document.querySelector('#Bg3');
+    element.classList.toggle("Dm4");
+
+    var element = document.querySelector('#Bg4');
+    element.classList.toggle("Dm5");
+}
